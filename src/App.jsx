106291c1f13a98b889d0b7303dd7610984a6e256
SHAVIK821307 <shavikmmp@gmail.com>
2024-11-21 00:25:31 +0530

    fabicon added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div className="mx-4 sm:mx-[10%] min-h-screen ">
+      <div className="mx-4 sm:mx-[10%] min-h-screen overflow-hidden ">
         <Navbar></Navbar>
 
         <div className="mx-0 h-full">
